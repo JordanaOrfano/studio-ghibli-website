fe0a963a033d67f16ee901dd35c04a46ba74872f
JordanaOrfano <jordanaorfano@gmail.com>
2023-01-15 02:54:35 -0300

    Add scroll to top button

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Scroll to top button
+function scrollToTop() {
+	window.scrollTo(0, 0);
+}
+
 // Copyright
 let date = new Date().getFullYear();
 document.getElementById('copyright-year').innerHTML = date;
